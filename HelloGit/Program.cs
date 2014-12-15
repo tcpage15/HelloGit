8fbe4b8673770ab7b96b6fb8946f265d43b36897
Todd C. Page <page_todd@hotmail.com>
2014-12-15 16:00:25 -0600

    First commit of HelloGit

---
+++ HelloGit/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Display();
+        }
+
+        public static void Display()
+        {
+            // Write out to the screen
+            Console.ReadLine();
+            Console.Write("Hello Git!");
+        }
+    }
+}
